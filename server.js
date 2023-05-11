501729f8c2eb6e047dfdb6d802393c0538034f35
Ruth-Kilian <127222133+Ruth-Kilian@users.noreply.github.com>
2023-05-11 20:40:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,12 @@
 
 const app = require("./app");
 const port = process.env.PORT || 8080;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+});
 
 app.listen(port, () => console.log("Server started on port 8080"));
